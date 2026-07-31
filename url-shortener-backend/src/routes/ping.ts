70862a25b4426d98681d3a5d502dc2efa377e9bd
ejdhindsa <ejdhindsa@Gmail.com>
2026-07-30 21:30:33 -0500

    implement POST /shorten with integration tests

--- url-shortener-backend/src/routes/ping.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-export async function pingRoutes(fastify: FastifyInstance) {
-  fastify.get("/ping", async (request, reply) => {
-    return { status: "ok" };
-  });
-}
