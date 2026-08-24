e1cbb83518127b1bcb4259b4aa9d3e99a11ca359
Ekamjot Singh <141810307+ejdhindsa@users.noreply.github.com>
2026-08-23 20:08:47 -0500

    feature(router): setup router and create skeleton views (#17)

--- url-shortener-frontend/src/main.ts
+++ url-shortener-frontend/src/main.ts
@@ -5,9 +5,11 @@ import "@unwreck/core/css";
 import "@unwreck/core/fonts.css";
 import "./style.scss";
 import App from "./App.vue";
+import router from "./router";
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
+app.use(router);
 app.mount("#app");
